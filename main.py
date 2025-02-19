fcb72110e56befaa95b386bfe9508cbd2bd78197
Arnold Obiero <odhisoarnold@gmail.com>
2025-02-19 09:17:41 +0300

    add calculator logic

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello python")
+from calculator import add, subtract, multiply, divide
+
+def main():
+    print("hello python")
+    print("Addition: ", add(5, 3))
+    print("Subtraction: ", subtract(5, 3))
+    print("Multiplication: ", multiply(5, 3))
+    print("Division: ", divide(5, 3))
+
+if __name__ == "__main__":
+    main()
